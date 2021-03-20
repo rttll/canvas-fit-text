6591a83504de25ae34d3b95e6ce67e2da50d7e8c
Adro <acousens@gmail.com>
2021-03-20 13:09:35 -0700

    Add multi-line support

--- CanvasFitText.js
+++ CanvasFitText.js
@@ -1,73 +1,103 @@
+CanvasRenderingContext2D.prototype.fitText = function (
+  text,
+  x,
+  y,
+  width,
+  height
+) {
+  var _this = this,
+    baseFontSize = 1,
+    fontSize = baseFontSize,
+    widthChangePerFontSize,
+    lineHeight = 1.2;
 
-CanvasRenderingContext2D.prototype.fitText = function(text, x, y, width, height) {
-  var _this = this;
-  var baseFontSize = 1
-  var fontSize = baseFontSize
-  var widthChangePerFontSize;
-  var lines = []
-
-  function resetFont() {
-    _this.font = `${baseFontSize}px sans-serif`
-  }
-
-  function lineHeight() {
-    return _this.fontSize * 1.2
-  }
-
-  function metrics(text) {
-    let m = _this.measureText(text)
+  function getMetrics(text) {
+    let m = _this.measureText(text);
     return {
-      width: (Math.abs(m.actualBoundingBoxLeft) + Math.abs(m.actualBoundingBoxRight)),
-      height: (Math.abs(m.fontBoundingBoxAscent) + Math.abs(m.fontBoundingBoxDescent))
+      width:
+        Math.abs(m.actualBoundingBoxLeft) + Math.abs(m.actualBoundingBoxRight),
+      height:
+        Math.abs(m.fontBoundingBoxAscent) + Math.abs(m.fontBoundingBoxDescent),
+    };
+  }
+
+  function calcArea() {
+    let metrics = getMetrics(text);
+    let rowHeight = metrics.height + fontSize * lineHeight;
+    return metrics.width * rowHeight;
+  }
+
+  function setFontSizeByArea() {
+    let currArea = calcArea();
+    while (currArea < width * height) {
+      currArea = calcArea();
+      fontSize++;
+      _this.font = `${fontSize}px sans-serif`;
     }
   }
 
-  // TODO multi-line text
-  function fitBox() {}
-
-  function calcWidth() {
-    let widthDiff = parseFloat(width - metrics(text).width)
-    let fontIncrease = widthDiff / widthChangePerFontSize
-    _this.font = `${fontSize + fontIncrease}px sans-serif`
+  function setFontSizeByWidth() {
+    let widthDiff = parseFloat(width - getMetrics(text).width),
+      fontIncrease = widthDiff / widthChangePerFontSize;
+    _this.font = `${fontSize + fontIncrease}px sans-serif`;
   }
 
-  function writeText() {
-    for ( let line of lines ) {
-      _this.fillText(line, x, y)
-      y += lineHeight()
+  function getTextToLines() {
+    let lines = [],
+      line = "",
+      words = text.split(" ");
+    while ((word = words.shift())) {
+      let lineMetrics = getMetrics(line),
+        wordMetrics = getMetrics(` ${word}`);
+      if (lineMetrics.width + wordMetrics.width < width) {
+        line += ` ${word}`;
+      } else {
+        lines.push(line.trim());
+        line = word;
+      }
+    }
+    lines.push(line.trim());
+    return lines;
+  }
+
+  function writeText(lines) {
+    _this.strokeStyle = "red";
+    _this.strokeRect(x, y, width, height);
+
+    for (let line of lines) {
+      _this.fillText(line, x, y);
+      y += lineHeight * fontSize;
     }
   }
-  
-  (function() {
 
-    if ( width === undefined ) {
-      throw new Error('CanvasFitText: width is required')
+  function calcWidthChangePerFontSize() {
+    let metrics = getMetrics(text),
+      startWidth = metrics.width;
+
+    _this.font = `${fontSize + 1}px sans-serif`;
+    widthChangePerFontSize = getMetrics(text).width - startWidth;
+
+    // Reset font or calcWidth() will not increase enough
+    _this.font = `${baseFontSize}px sans-serif`;
+  }
+
+  (function () {
+    if (width === undefined) {
+      throw new Error("CanvasFitText: width is required");
     }
-
-    // console.log(`starting at ${metrics(text).width}`)
 
     // Set starting font size, so we're always increasing the font up to reach target width
     // i.e. Can ignore if the starting width is higher than target width
-    _this.font = `${baseFontSize}px sans-serif`
-
-    // Calculate width change per font size
-    let startWidth = metrics(text).width
-    _this.font = `${fontSize+1}px sans-serif`
-    widthChangePerFontSize = metrics(text).width - startWidth
-    // Reset font or calcWidth() will not increase enough
-    resetFont()
-    
+    _this.font = `${baseFontSize}px sans-serif`;
 
     if (height) {
-      fitBox()
+      setFontSizeByArea();
+      var lines = getTextToLines();
+      writeText(lines);
     } else {
-      calcWidth()
-      lines.push(text)
+      calcWidthChangePerFontSize();
+      setFontSizeByWidth();
+      writeText([text]);
     }
-    
-    writeText()
-
-    // console.log(`ending at ${metrics(text).width}`)
-
-  })()
-}
+  })();
+};
